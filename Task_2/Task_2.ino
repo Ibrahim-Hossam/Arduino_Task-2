8265603e88a3541e3744fd89d05d98a86a55edb8
Ibrahim-Hossam <himaswim@gmail.com>
2021-09-08 17:17:41 +0200

    Added comments (final)

--- Task_2/Task_2.ino
+++ Task_2/Task_2.ino
@@ -1,12 +1,12 @@
-#define Red 8 
-#define Green 10
-#define buzzer 5
-int sensorPin = 0;
-int sensorPin_2 = 1;
-int sensorVal = 0;
-int sensorVal_2 = 0;
-int number_of_persons = 0;
-void setup() { Serial.begin(9600);
+#define Red 8 // Red led pin
+#define Green 10  // Green led pin
+#define buzzer 5  // buzzer pin
+int EntrPin = 0;  // Enterance light sensor input pin
+int ExitPin = 1;  // Exit light sensor input pin
+int EntrVal = 0;  // initializing the light sensor value
+int ExitVal = 0;
+int number_of_persons = 0;   // initializing the number of persons variable to count the number of persons in a  room
+void setup() { Serial.begin(9600); // In wetup we Gave each pin it's information as input or output
 pinMode(Green,OUTPUT);
 pinMode(Red,OUTPUT);
 pinMode(buzzer,OUTPUT);
@@ -14,24 +14,23 @@ pinMode(A0,INPUT);
 pinMode(A1,INPUT);
 }
 void loop() {
-Serial.print(number_of_persons,"\n");
-sensorVal = analogRead(sensorPin);
-sensorVal_2 = analogRead(sensorPin_2);
-if ( sensorVal < 300){
+EntrVal = analogRead(EntrPin);  // A variable to read the entrance light sensor value 
+ExitVal = analogRead(ExitPin);  // A variable to read the entrance light sensor value 
+if ( EntrVal < 300){  // A condition to count the number of persons entered if the light sensor value is less than the room light and to make the green led turn on to know that a person entered(the maximum value of the sensor is 550)
   digitalWrite(Green, HIGH);
   number_of_persons ++;
   delay(1500);
 } else{
   digitalWrite(Green, LOW);
 }
-if ( sensorVal_2 < 400){
+if ( ExitVal < 400){  // A condition to count the number of persons exited if the light sensor value is less than the room light and to make the red led turn on to know that a person exited(the maximum value of the sensor is 650)
   digitalWrite( Red, HIGH);
   number_of_persons --;
   delay(1500);
 } else{
   digitalWrite(Red, LOW);
 }
- if (number_of_persons > 5){
+ if (number_of_persons > 5){  // A condition to make the buzzer make sound and to show an error message if the number of persons in a room is more than 5
       Serial.print("ERROR: Number of persons exceeded 5 persons in the room!\n");
       tone(buzzer, 1000);
     }
